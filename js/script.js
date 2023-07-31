bc946b24585b1c4980843edb0bfc6cb48a46f619
ChongChiSengVincent <123968292+ChongChiSengVincent@users.noreply.github.com>
2023-07-31 13:34:22 +0800

    added breakpoints & example code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const page1btn=document.querySelector("#homeButton");
 const page2btn=document.querySelector("#cppButton");
 const page3btn=document.querySelector("#pyButton");
+const homeCalcButton = document.querySelectorAll(".homeCalcButton")[0];
+const homeCalcButton2 = document.querySelectorAll(".homeCalcButton")[1];
+const code = document.querySelectorAll(".Code")[0];
+const code2 = document.querySelectorAll(".Code")[1]
 var allpages=document.querySelectorAll(".page");
 //select all subtopic pages
 console.log(allpages);
@@ -18,7 +22,15 @@ function show(pgno){ //function to show selected page no
     //show the page
     onepage.style.display="block";
 }
-
+function showExample(){
+    console.log("hi");
+    code.innerHTML = "<div>Answer : 10</div>";
+}
+function showCalculationExample(){
+    var a = parseInt(document.getElementById("calc1").value);
+    var b = parseInt(document.getElementById("calc2").value);
+    code2.innerHTML = "<div>Answer : "+ String(a+b) + "</div>";
+}
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
 if (page1btn != null)
@@ -27,4 +39,11 @@ if (page2btn != null)
 page2btn.addEventListener("click", function () { show(1); });
 if (page3btn != null)
 page3btn.addEventListener("click", function () { show(2); });
+if (homeCalcButton != null){
+    homeCalcButton.addEventListener("click", function() { showExample(); });
+}
+if (homeCalcButton2 != null){
+    homeCalcButton2.addEventListener("click", function() { showCalculationExample(); });
+}
+show(0);
 
